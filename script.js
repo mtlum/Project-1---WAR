c363a792c08efc3ec95ed179dcd2c81b42eab66b
Michael Lum <michaellum@Michaels-MacBook-Pro.local>
2022-06-17 09:46:58 -0400

    cleaned up code

--- script.js
+++ script.js
@@ -34,14 +34,12 @@ function playing() {
     dealerHand = dCardValue();
     dealerSuit = dCardSuit();
     dealerCard = dCardValueSuit();
-    // cardGen();
     winCheck(playerHand, dealerHand);
   } else if (credit === 0) {
     credits = `Not enough credits! ${credit}`;
     document.getElementById("credits").innerHTML = credits;
     gameover = `GAME OVER`;
     document.getElementById("outcome").innerHTML = gameover;
-    // alert("Not enough credits. GAME OVER!");
     return;
   }
   credits = `Credits: ${credit}`;
@@ -62,21 +60,11 @@ function pCardSuit() {
   return pSuit;
 }
 
-// var cardHolder = document.querySelector(".card");
-
-// function cardGen() {
-//   // let divEl = document.createElement("div");
-//   // divEl.classList.add("card", playerCard);
-//   cardHolder.classList.add("card", playerCard);
-//   // cardHolder.appendChild(divEl);
-// }
-
 //player card
 function pCardValueSuit() {
   pCard = pSuit + pValue;
   console.log(pCard);
   document.getElementById("presult").innerHTML = pCard;
-  // cardHolder.classList.add("card", playerCard);
   return pCard;
 }
 
@@ -93,51 +81,36 @@ function dCardSuit() {
   console.log(dSuit);
   return dSuit;
 }
-// var cardDHolder = document.querySelector(".card");
 
 //dealer card
 function dCardValueSuit() {
   dCard = dSuit + dValue;
   console.log(dCard);
   document.getElementById("dresult").innerHTML = dCard;
-  // cardHolder.classList.add("card", dealerCard);
   return dCard;
 }
-
-// var win;
-// var lose;
-// var tie;
 
 // Win Check
 function winCheck(pCardValue, dCardValue) {
   console.log(pCardValue, dCardValue);
   if (pCardValue > dCardValue) {
-    credit += 2; //increase credit by 1 if player wins
-    // return "Player Wins!";
+    credit += 2;
     console.log("Player Wins!");
     win = `Player Wins!`;
     document.getElementById("outcome").innerHTML = win;
   } else if (pCardValue < dCardValue) {
-    // credit -= 1; // decrease credit by 1 if player loses
-    // return "Dealer Wins!";
     console.log("Dealer Wins!");
     lose = `Dealer Wins!`;
     document.getElementById("outcome").innerHTML = lose;
   } else if (pCardValue === dCardValue) {
-    //return "WAR!";
     console.log("WAR!");
     tie = `WAR!`;
     document.getElementById("outcome").innerHTML = tie;
     setTimeout(function () {
       war();
     }, 1500);
-
-    // war();
   }
 }
-
-// let outcome = `${winCheck()}`;
-// document.getElementById("outcome").innerHTML = outcome;
 
 // war, get new cards
 function war() {
@@ -147,31 +120,23 @@ function war() {
   dealerSuit2 = dCardSuit();
   dealerHand2 = dCardValue();
   dealerCard2 = dCardValueSuit();
-  // document.getElementById("d2result").innerHTML = dCard;
   warCheck(playerHand2, dealerHand2);
 }
-// let pwcard = `${dealerCard()}`;
-// document.getElementById("pwcard").innerHTML = pwcard;
-// let dwcard = `${dealerCard()}`;
-// document.getElementById("dwcard").innerHTML = dwcard;
 
 //war, check new cards
 function warCheck() {
   console.log(playerHand2, dealerHand2);
   if (playerHand2 > dealerHand2) {
-    credit += 3; //increase credit by 3 for war win
-    // return "Player Wins War!";
+    credit += 3;
     win = `Player Wins WAR!`;
     document.getElementById("outcome").innerHTML = win;
     console.log("Player Wins WAR!");
   } else if (playerHand2 < dealerHand2) {
-    credit -= 1; //decrase credit by 2 for loss
-    // return "Dealer Wins War!";
+    credit -= 1;
     lose = `Dealer Wins WAR!`;
     document.getElementById("outcome").innerHTML = lose;
     console.log("Dealer Wins WAR!");
   } else if (playerHand2 === dealerHand2) {
-    // return "WAR!";
     tie = `WAR!`;
     document.getElementById("outcome").innerHTML = tie;
     console.log("WAR!");
